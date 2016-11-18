c4f53cd4ec6e04dd2e14e14acc2e14255f976484
Giorgio Balduzzi <gio.balduz@gmail.com>
2016-11-18 09:27:28 +0100

    Aggiunto caso d'errore all'es5 (equals) e aggiunta versione della classe senza errori

--- Exe2_java/src/es5/Equals.java
+++ Exe2_java/src/es5/Equals.java
@@ -14,11 +14,26 @@ public class Equals {
 		// TODO Auto-generated method stub
 		Persona p1 = new Persona("GMBDNL12345");
 		Persona p2 = new Persona("GMBDNL12345");
-		Persona o = new Persona("GMBDNL12345");
+		Persona p3 = new Persona("GMBDNL12345");
+		
+		//Finchè fai equals tra Persona l'equals così è corretto
 		
 		System.out.print("p1.equals(p2) ? ");System.out.println(p1.equals(p2));
-		System.out.print("o.equals(p1) ? ");System.out.println(o.equals(p1));
-		System.out.print("p1.equals(o) ? ");System.out.println(p1.equals(o));
+		System.out.print("p3.equals(p1) ? ");System.out.println(p3.equals(p1));
+		System.out.print("p1.equals(p3) ? ");System.out.println(p1.equals(p3));
+		
+		Object o = new Persona("GMBDNL12345");
+		// Falso anche se gli oggetti sono entrambi Persona() con stesso cf
+		System.out.print("p1.equals(o) ? ");System.out.println(p1.equals(o)); 
+		
+		Persona_correct pc1 = new Persona_correct("GMBDNL12345");
+		Persona_correct pc2 = new Persona_correct("GMBDNL12345");
+		Object o2 = new Persona_correct("GMBDNL12345");
+		//Tutto corretto
+		System.out.print("pc1.equals(pc2) ? ");System.out.println(pc1.equals(pc2));
+		System.out.print("pc1.equals(o2) ? ");System.out.println(pc1.equals(o2));
+		System.out.print("o2.equals(pc2) ? ");System.out.println(o2.equals(pc2));
+		
 	}
 
 }
@@ -34,6 +49,24 @@ class Persona{
 	//in questo modo due persone con lo stesso cf potrebbero non essere equals se viene chiamato equals(o) invece di equals(p)
 	public boolean equals(Persona p) {
 		return this.cf.equals(p.cf);
+	}
+	
+}
+
+class Persona_correct{
+	String nome;
+	String cf;
+	
+	public Persona_correct(String cf) {
+		this.cf = new String(cf);
+	}
+	
+	public boolean equals(Object o) {
+		if (o instanceof Persona_correct) {
+			return this.cf.equals(((Persona_correct)o).cf);
+		} else {
+			return o.equals(this);
+		}
 	}
 	
 }
